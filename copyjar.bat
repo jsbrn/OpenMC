784b2565b249fda608f2f21e5f18a8431c7fe0c1
Jeremy <23155614+jsbrn@users.noreply.github.com>
2019-02-17 17:52:18 -0500

    Streamlined build management

--- copyjar.bat
+++ copyjar.bat
@@ -1,9 +1,9 @@
 echo off
-set source="C:\Users\Main\Documents\GitHub\OpenMC\target"
-set target="C:\Users\Main\Documents\OpenMCTestServer\plugins"
+set folder=%1
+set target="C:\Users\Jeremy\Documents\Github\OpenMC\target"
 echo Deleting OpenMC.jar
 del %source%\OpenMC.jar
 echo Renaming shaded jar
-ren %source%\OpenMC-dev-shaded.jar OpenMC.jar
+ren %source%\original-OpenMC.jar OpenMC.jar
 echo Copying to plugins folder
 robocopy %source% %target% OpenMC.jar /MOV
